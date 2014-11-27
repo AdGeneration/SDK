274d1197281bb0465b6ada7589e03029a236e7a6
AdGeneration <ssp-ope@mediba.jp>
2014-11-27 13:16:24 +0900

    AdMobメディエーション1.0.0リリース

---
+++ ADG_AdMobMediation/iOS/ADGAdMobMediation.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import "GADCustomEventBanner.h"
+#import "GADCustomEventBannerDelegate.h"
+#import "ADGManagerViewController.h"
+
+@interface ADGAdMobMediation : NSObject<GADCustomEventBanner , ADGManagerViewControllerDelegate>
+
+@property (nonatomic) ADGManagerViewController *adg;
+@property(nonatomic, unsafe_unretained) NSObject<GADCustomEventBannerDelegate> *delegate;
+
+@end
