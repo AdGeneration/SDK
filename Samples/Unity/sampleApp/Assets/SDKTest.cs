c99753fe869c38541050e87cc3fc12eed685aa4e
shinriyo <shinriyo@gmail.com>
2015-03-22 22:43:58 +0900

    コード整形
    コード整形

--- Samples/Unity/sampleApp/Assets/SDKTest.cs
+++ Samples/Unity/sampleApp/Assets/SDKTest.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using System.Collections;
 
-public class SDKTest : MonoBehaviour {
+public class SDKTest : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		ADGUnitySDK.IOSLocationID = "10723";
 		ADGUnitySDK.AndroidLocationID = "10724";
 		ADGUnitySDK.AdType = "SP";
@@ -18,10 +20,11 @@ public class SDKTest : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	}
 	
-	void OnGUI()
+	void OnGUI ()
 	{
 		if (GUI.Button(new Rect(10, 170, 100, 50), "show"))
 		{
@@ -37,7 +40,7 @@ public class SDKTest : MonoBehaviour {
 		{
 			
 			if(Application.platform == RuntimePlatform.IPhonePlayer){
-				ADGUnitySDK.changeLocationADG( 0 , 350);
+				ADGUnitySDK.changeLocationADG(0, 350);
 			} 
 			else if(Application.platform == RuntimePlatform.Android){
 				ADGUnitySDK.changeLocationADG("LEFT" , "BOTTOM");
@@ -47,37 +50,44 @@ public class SDKTest : MonoBehaviour {
 		if (GUI.Button(new Rect(120, 270, 100, 50), "Location2"))
 		{
 			
-			if(Application.platform == RuntimePlatform.IPhonePlayer){
-				ADGUnitySDK.changeLocationADG( 0 , 0);
+			if (Application.platform == RuntimePlatform.IPhonePlayer)
+			{
+				ADGUnitySDK.changeLocationADG(0 , 0);
 			}
-			else if(Application.platform == RuntimePlatform.Android){
-				ADGUnitySDK.changeLocationADG("LEFT" , " TOP");
+			else if (Application.platform == RuntimePlatform.Android)
+			{
+				ADGUnitySDK.changeLocationADG("LEFT", " TOP");
 			}
 		}
 	}
 	
 	//recieve mesasge
-	void ADGReceiveAd(string str){
+	void ADGReceiveAd (string str)
+	{
 		Debug.Log(str);
 	}
 	
-	void ADGFailedToReceiveAd(string str){
+	void ADGFailedToReceiveAd(string str)
+	{
 		Debug.Log(str);
 	}
 	
-	void ADGBrowserShow(string str){
+	void ADGBrowserShow (string str)
+	{
 		Debug.Log(str);
 	}
 	
-	void ADGBrowserClose(string str){
+	void ADGBrowserClose (string str){
 		Debug.Log(str);
 	}
 	
-	void ADGVideoShow(string str){
+	void ADGVideoShow (string str)
+	{
 		Debug.Log(str);
 	}
 	
-	void ADGVideoDisappear(string str){
+	void ADGVideoDisappear (string str)
+	{
 		Debug.Log(str);
 	}
 }
